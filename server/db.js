9a187e69e2ad56d78cbb22d23ec9a7e2a91421a9
AbhinavDev18 <abhinav00557@gmail.com>
2025-10-07 09:29:29 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || "mongodb+srv://abhinav1:abhinav118@cluster0.8vyve.mongodb.net/votingSystem";
+  const uri = process.env.MONGO_URI;
   try {
     await mongoose.connect(uri);
     console.log("✅ MongoDB connected");
